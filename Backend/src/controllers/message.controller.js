0e868218a90245b24710758c90e4426f0f62dd52
Krish Kumar <krish.k2024@gmail.com>
2025-04-14 11:45:12 +0530

    messageController

--- Backend/src/controllers/message.controller.js
+++ Backend/src/controllers/message.controller.js
@@ -1,11 +1,15 @@
 import messageModel from '../models/message.model.js'
+import userModel from '../models/user.model.js'
 import { Types } from 'mongoose'
+import { Translation } from '../lib/groq.js';
 
 export const sendMessageRoute = async (req, res) => {
     try {
         const senderId = req.user?._id;
         const receiverId = req.params.id;
+        const langSelected = req.user.selectedLanguage
         const { originalText } = req.body;
+
 
         if (!originalText) {
             res.status(400).json({
@@ -21,16 +25,31 @@ export const sendMessageRoute = async (req, res) => {
             return;
         }
 
-        const msg = await messageModel.create({
-            senderId,
-            receiverId: new Types.ObjectId(receiverId),
-            originalText
-        });
+        if (langSelected === 'en') {
+            const msg = await messageModel.create({
+                senderId,
+                receiverId: new Types.ObjectId(receiverId),
+                originalText
+            });
 
-        res.status(200).json({
-            message: "Message sent successfully",
-            msg
-        });
+            res.status(200).json({
+                message: "Message sent successfully",
+                msg
+            });
+        } else {
+            const trans = await Translation(originalText, "en")
+            const msg = await messageModel.create({
+                senderId,
+                receiverId: new Types.ObjectId(receiverId),
+                originalText: trans['en']
+            })
+            res.status(200).json({
+                message: "Message send successfully",
+                msg
+            })
+        }
+
+
 
     } catch (error) {
         if (error instanceof Error) {
@@ -43,5 +62,72 @@ export const sendMessageRoute = async (req, res) => {
                 message: "Something went wrong!"
             });
         }
+    }
+}
+
+export const updateLang = async (req, res) => {
+    try {
+        const userId = req.user._id;
+        const lang = req.body.lang
+        const updatedLanguage = await userModel.findByIdAndUpdate(userId, {
+            selectedLanguage: lang
+        },
+            {
+                new: true,
+                runValidators: true
+            }
+        )
+        res.status(200).json({
+            updatedLanguage
+        })
+
+
+    } catch (error) {
+        console.log(`error in update lang ${error.message}`)
+        res.status(400).json({
+            message: "Internal Server Erro "
+        })
+    }
+}
+
+
+export const getMessages = async (req, res) => {
+    try {
+        const userId = req.user._id;
+        const receiverId = req.params.id;
+        const langSelected = req.user.selectedLanguage;
+
+        const orgMessage = await messageModel.find({
+            $or: [
+                { senderId: receiverId, receiverId: userId },
+                { senderId: userId, receiverId: receiverId }
+            ]
+        });
+
+        if (langSelected === "en") {
+            return res.status(200).json(orgMessage);
+        }
+
+        const translatedMessages = await Promise.all(
+            orgMessage.map(async (message) => {
+                if (!message.translatedText || !message.translatedText[langSelected]) {
+                    const translation = await Translation(message.originalText, langSelected);
+                    const updatedMessage = await messageModel.findByIdAndUpdate(
+                        message._id,
+                        { translatedText: translation },
+                        { new: true }
+                    );
+                    return updatedMessage;
+                }
+                return message;
+            })
+        );
+
+        res.status(200).json(translatedMessages);
+    } catch (error) {
+        console.error("Error in getMessages:", error);
+        res.status(500).json({
+            message: "Internal Server Error"
+        });
     }
 }
