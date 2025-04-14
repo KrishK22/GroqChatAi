0f8ab04888b1557e515d37eba00448aec9794ee7
Krish Kumar <krish.k2024@gmail.com>
2025-04-14 11:43:01 +0530

    messageRouteUpdate

--- Backend/src/routes/message.route.js
+++ Backend/src/routes/message.route.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { sendMessageRoute } from '../controllers/message.controller.js'
+import { sendMessageRoute, getMessages ,updateLang} from '../controllers/message.controller.js'
 import { protectedRoute } from "../middleware/protectedRoute.js";
 
 const router = express.Router();
 
 router.post('/send-message/:id', protectedRoute, sendMessageRoute)
+router.patch('/updateLanguage', protectedRoute, updateLang)
+router.get('/getMessages/:id', protectedRoute, getMessages)
 
 
 export default router
